f727f8f20052b4ca6628b1d6b0573f374c50a45a
musou1500 <musou1500@gmail.com>
2019-03-11 10:25:55 +0900

    refactor

--- perceptron.mjs
+++ perceptron.mjs
@@ -3,50 +3,71 @@ import math from 'mathjs';
 const getRandIdx = examples =>
   Math.floor(Math.random() * examples.length);
 
-const isCorrect = (s, ans, idx) =>
-  s > 0 && ans[idx] > 0 || s < 0 && ans[idx] < 0;
+const pickRand = (examples, classifications) => {
+  const randIdx = getRandIdx(examples);
+  return {
+    example: examples[randIdx],
+    classification: classifications[randIdx]
+  };
+};
 
-export const learn = (examples, ans) => {
-  if (examples.length <= 0) {
-    throw new RangeError('give >1 examples');
-  }
+const isCorrect = (s, classification) =>
+  s > 0 && classification > 0 || s < 0 && classification < 0;
 
-  let weight = [...new Array(examples[0].length)].fill(0);
-  let count = 0;
-  let run = 0;
-  const pocket = {
-    weight: [...weight],
-    count: 0,
+const createIterWithPocket = weightLen => {
+  const iteration = {
+    weight: getInitWeight(weightLen),
+    okNum: 0,
     run: 0
   };
 
-  let i = 0;
-  const countLimit = 999;
+  const pocket = {
+    weight: getInitWeight(weightLen),
+    okNum: 0,
+    run: 0
+  };
 
-  while(i++ < countLimit) {
-    const randIdx = getRandIdx(examples);
-    const example = examples[randIdx];
-    const s = math.multiply(weight, example);
-    if (isCorrect(s, ans, randIdx)) {
-      run++;
+  return { iteration, pocket };
+};
+
+const getInitWeight = len => [...new Array(len)].fill(0);
+
+const testWeight = (weight, example, classification) =>
+  isCorrect(math.multiply(weight, example), classification);
+
+export const learn = (examples, classifications, maxIter = 999) => {
+  if (examples.length <= 0) {
+    throw new RangeError('give >0 examples');
+  }
+
+  if (examples.length !== classifications.length) {
+    throw new RangeError('examples must have same length as classification');
+  }
+
+  const { iteration, pocket } = createIterWithPocket(examples[0].length);
+
+  let i = 0;
+  while(i++ < maxIter) {
+    const { example, classification } = pickRand(examples, classifications);
+    if (testWeight(iteration.weight, example, classification)) {
+      iteration.run++;
 
       // 正しく分類できる数
-      count = examples
-        .map(e => math.multiply(weight, e))
-        .map((s, i) => isCorrect(s, ans, i) ? 1 : 0)
-        .reduce((prev, cur) => prev + cur);
+      iteration.okNum = examples
+        .map((e, i) => testWeight(iteration.weight, e, classifications[i]) ? 1 : 0)
+        .reduce((prev, cur) => prev + cur, 0);
 
-      if (count > pocket.count) {
-        pocket.count = count;
-        pocket.run = run;
-        pocket.weight = weight;
-        if (count === examples.length) {
+      if (iteration.okNum > pocket.okNum) {
+        pocket.okNum = iteration.okNum;
+        pocket.run = iteration.run;
+        pocket.weight = iteration.weight;
+        if (iteration.okNum === examples.length) {
           break;
         }
       }
     } else {
-      run = 0;
-      weight = math.add(weight, math.multiply(ans[randIdx], example));
+      iteration.okNum = 0;
+      iteration.weight = math.add(iteration.weight, math.multiply(classification, example));
     }
   }
 
